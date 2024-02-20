fcb6b6c0110101a4aa6fa3905e58c54ee71a47b6
Fabiorincont12 <160671962+Fabiorincont12@users.noreply.github.com>
2024-02-20 16:49:52 -0500

    Add files via upload

--- buscar_correo.php
+++ buscar_correo.php
@@ -1,14 +1,14 @@
 <?php
 // Datos de conexión a Airtable
-$api_key = 'TU_API_KEY';
-$base_id = 'ID_DE_TU_BASE';
-$table_name = 'Nombre_de_tu_tabla';
+$api_key = 'pat7NiF8slEtqShQB.e9aa80b117b098186e66a6736ab9db0871e1d788d5dec89634c4f03d74326087';
+$base_id = 'appoXRlwse5BNcjNS';
+$table_name = 'Sheet0';
 
 // Obtener identificación del formulario
-$identificacion = $_POST['identificacion'];
+$identificacion = $_POST['Field 2'];
 
 // URL de la solicitud a Airtable para obtener los datos del estudiante por identificación
-$url = "https://api.airtable.com/v0/{$base_id}/{$table_name}?filterByFormula=({identificacion}='{$identificacion}')";
+$url = "https://api.airtable.com/v0/{$base_id}/{$table_name}?filterByFormula=({Identificacion}='{$identificacion}')";
 
 // Configurar la solicitud HTTP
 $options = [
@@ -26,7 +26,7 @@ $data = json_decode($response, true);
 // Verificar si se encontraron registros
 if (!empty($data['records'])) {
     // Obtener el correo asociado a la identificación
-    $correo = $data['records'][0]['fields']['correo'];
+    $correo = $data['records'][0]['Field 2']['correo'];
     echo "El correo asociado a la identificación $identificacion es: " . $correo;
 } else {
     echo "No se encontró ningún correo asociado a la identificación $identificacion";
